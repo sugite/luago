91cf712dced46fc0e38bcec09cf6f07919f41d3e
dengbo <dengbo.live@bytedance.com>
2022-06-24 15:13:17 +0800

    指令解码

--- go/ch03/src/luago/vm/instruction.go
+++ go/ch03/src/luago/vm/instruction.go
@@ -3,6 +3,20 @@ package vm
 const MAXARG_Bx = 1<<18 - 1       // 2^18 - 1 = 262143
 const MAXARG_sBx = MAXARG_Bx >> 1 // 262143 / 2 = 131071
 
+/*
+ 31       22       13       5    0
+  +-------+^------+-^-----+-^-----
+  |b=9bits |c=9bits |a=8bits|op=6|
+  +-------+^------+-^-----+-^-----
+  |    bx=18bits    |a=8bits|op=6|
+  +-------+^------+-^-----+-^-----
+  |   sbx=18bits    |a=8bits|op=6|
+  +-------+^------+-^-----+-^-----
+  |    ax=26bits            |op=6|
+  +-------+^------+-^-----+-^-----
+ 31      23      15       7      0
+*/
+
 type Instruction uint32
 
 func (ins Instruction) Opcode() int {
@@ -10,9 +24,9 @@ func (ins Instruction) Opcode() int {
 }
 
 func (ins Instruction) ABC() (a, b, c int) {
-	a = int(ins >> 6 & 0xFF)
-	b = int(ins >> 14 & 0x1FF)
-	c = int(ins >> 23 & 0x1FF)
+	a = int((ins >> 6) & 0xFF)
+	c = int((ins >> 14) & 0x1FF)
+	b = int((ins >> 23) & 0x1FF)
 	return
 }
 
